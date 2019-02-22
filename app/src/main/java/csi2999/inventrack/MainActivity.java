66e2d017b485538ec0f9f1d6cbbe594e2aa080b5
Jacob <jggianakopoulos@gmail.com>
2019-02-22 18:11:54 -0500

    Initial commit

---
+++ app/src/main/java/csi2999/inventrack/MainActivity.java
@@ -0,0 +1,13 @@
+package csi2999.inventrack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
