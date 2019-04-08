902b99901ec66353053c251369ad203c4cfbf754
CMadek <35314019+CMadek@users.noreply.github.com>
2019-04-07 21:10:51 -0400

    Add files via upload

---
+++ app/src/main/java/csi2999/inventrack/Formatter.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Formatter {  //utility class for converting an arraylist of all of our data into an arraylist of maps with the query column titles being the key and having a corresponding value
+    protected ArrayList<String> rawData;
+
+
+
+    protected ArrayList<HashMap> result;
+    protected ArrayList<String> fieldNames;
+    protected int qNum;
+
+    public Formatter(ArrayList<String> rawData, ArrayList<String> fieldNames){
+        this.rawData = rawData;
+        this.fieldNames = fieldNames;
+        this.qNum = rawData.size()/fieldNames.size();
+        result = new ArrayList<>();
+
+    }
+    public HashMap<String, String> mapvalue(int multiplicand){
+        HashMap<String, String> mappedData = new HashMap<String, String>();
+        int startVal = multiplicand * fieldNames.size();
+        for (int i = 0; i < fieldNames.size(); i++) {
+            mappedData.put(fieldNames.get(i), rawData.get(startVal));
+            //System.out.println(startVal);
+            startVal++;
+        }
+
+        return  mappedData;
+    }
+
+
+    public ArrayList<HashMap> getResult() {
+        HashMap<String, String> x = new HashMap<>();
+        for(int i = 0; i < qNum - 1; i++) {
+            x = mapvalue(i);
+            result.add(x);
+        }
+        return result;
+    }
+}
---
+++ app/src/main/java/csi2999/inventrack/SQLQuerier.java
@@ -0,0 +1,112 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class SQLQuerier {
+
+    protected String query; // the full query which will be concatanaed from command, number and table
+    protected String command; //where SELECT or DROP would go in a query
+    protected String table; //the table being queried
+    protected String number; // the number of rows to request
+    protected HashMap<String, String[]> qData;
+    protected Socket qSock; //The socket that makes a connection to the server
+    protected String host;
+    protected int port;
+
+
+    public SQLQuerier(String host, int port) throws IOException{
+        this.host = host;
+        this.port = port;
+        try {
+            this.connect();
+        }
+        catch (IOException e){
+            System.out.print("IOException");
+        }
+        this.query = null;
+    }
+
+
+
+    protected ArrayList<HashMap> query(String command, String number, String table, String rowCount) throws IOException {
+        //query selected table for # of columns returned, then query the server for all data requested, using querySort to create an arraylist
+        ArrayList<String> respList = new ArrayList<String>();
+        ArrayList<HashMap> result = new ArrayList<>();
+        PrintWriter sockOut = new PrintWriter(this.qSock.getOutputStream(), true);
+        BufferedReader sockIn = new BufferedReader(new InputStreamReader(this.qSock.getInputStream())); // instantiates the printwriter and buffered reader used to send data through a socket
+        int count = getColumnCount(sockOut, sockIn, table);
+        int respNum = count * Integer.parseInt(rowCount);
+
+        //System.out.println(respNum);
+        this.setQuery(command + " " + number + " from " + table + " LIMIT " + rowCount + ";");
+
+        sockOut.println(this.query);
+        respList = getResponse(sockIn, respNum);
+        ArrayList<String> tableData = queryforTableData(sockOut, sockIn, table, count);
+        result = getFormattedData(respList, tableData);
+        return result;
+    }
+
+    protected ArrayList<String> queryforTableData(PrintWriter sockOut, BufferedReader sockIn, String table, int respNum) throws IOException{
+        ArrayList<String> tableData = new ArrayList<String>();
+        sockOut.println("SELECT column_name from information_schema.columns WHERE table_name = '" + table + "'");
+        tableData = getResponse(sockIn, respNum);
+
+        return tableData;
+
+    }
+
+
+    public ArrayList<String> getResponse(BufferedReader sockIn, int respNum) throws IOException{
+        ArrayList<String> respList = new ArrayList<String>();
+        String response;
+        for(int i = 0; i < respNum; i++){
+            response = sockIn.readLine();
+            if(response.contains("TERMINATE"))
+                continue;
+            respList.add(response);
+
+        }
+        return  respList;
+    }
+
+    protected int getColumnCount(PrintWriter sockOut, BufferedReader sockIn, String table) throws IOException{ // returns the number of columns
+        int count;
+        try {
+            sockOut.println("SELECT COUNT(*) from information_schema.columns WHERE table_name = '" + table + "' ");
+            count = Integer.parseInt(sockIn.readLine());
+            return count;
+
+        }
+        catch (Exception e){
+            return 5;
+        }
+    }
+
+    protected ArrayList<HashMap> getFormattedData(ArrayList<String> respList, ArrayList<String> tableData){
+        ArrayList<HashMap> result = new ArrayList<>();
+        Formatter formatter = new Formatter(respList, tableData);
+        result = formatter.getResult();
+        return result;
+
+    }
+
+    private void connect() throws IOException { //connects us to the remote host
+        this.qSock = new Socket(this.host, this.port);
+        //System.out.print("Socket connected");
+    }
+
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
+}
